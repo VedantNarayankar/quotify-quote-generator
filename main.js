53996fc14ddcf418ae6b403fda2a2c85562773f1
VedantNarayankar <vedantnarayankar007@gmail.com>
2022-02-05 02:45:42 +0530

    first commit

--- main.js
+++ main.js
@@ -43,17 +43,3 @@ instagramBtn.addEventListener("click", ()=>{
 });
 
 quoteBtn.addEventListener("click", randomQuote);
-
-const express = require("express");
-
-const app = express();
-
-app.use(express.static("public"));
-
-app.get("/", function (req, res) {
-res.sendFile(__dirname+"/index.html") 
-});
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port"+process.env.PORT);
-});
